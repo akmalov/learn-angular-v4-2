b04b7ae1c16d6f8ea17ad5e150d3ac2014e57f9f
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-11 22:50:19 +0300

    learn template and stylesheets urls

---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sandbox',
+    templateUrl: './sandbox.component.html',
+    styleUrls: ['./sandbox.component.css']
+})
+
+export class SandboxComponent{
+}
