13bcacacc70236656e59cbe7c750ee3bbb515500
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-12 12:47:52 +0300

    learn ngfor directive

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -2,9 +2,48 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    templateUrl: './sandbox.component.html',
-    styleUrls: ['./sandbox.component.css']
+    template: `
+        <h1>Hello World</h1>
+        <ul>
+            <li *ngFor="let person of people">
+                {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people; let i = index">
+                {{i + 1}} {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people2">
+                {{person.firstName}} {{person.lastName}}
+            </li>
+        </ul>
+    `
 })
 
 export class SandboxComponent{
+    people = ['Rick', 'Daryl', 'Carl', 'Glen'];
+
+    people2 = [
+        {
+            firstName: 'Rick',
+            lastName: 'Grimes'
+        },
+        {
+            firstName: 'Daryl',
+            lastName: 'Trump'
+        },
+        {
+            firstName: 'Carl',
+            lastName: 'Clinton'
+        },
+        {
+            firstName: 'Glen',
+            lastName: 'Bush'
+        }
+    ];
+    constructor(){
+        this.people[2] = 'Carol';
+    }
 }
