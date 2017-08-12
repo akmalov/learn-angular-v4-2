cff3dc395c95c39b9c494826e4d494d7cdb17f19
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-12 15:09:35 +0300

    learn two way binding with ngmodel

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -4,42 +4,26 @@ import { Component } from '@angular/core';
     selector: 'sandbox',
     template: `
         <h1>Hello World</h1>
-        <div>
-            <input type="text" (keyup)="fireEvent($event)" placeholder="keyup">
+        <div class="container">
+            <form>
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" [(ngModel)]="name" name="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Age</label>
+                    <input type="number" [(ngModel)]="age" name="age" class="form-control">
+                </div>
+                <input type="submit" value="Submit" class="btn btn-success">
+            </form>
+            
+            <h4>Name: {{name}}</h4>
+            <h4>Age: {{ age }}</h4>
         </div>
-        <div>
-            <input type="text" (keydown)="fireEvent($event)" placeholder="keydown">
-        </div>
-        <div>
-            <input type="text" (focus)="fireEvent($event)" placeholder="focus">
-        </div>
-        <div>
-            <input type="text" (blur)="fireEvent($event)" placeholder="blur">
-        </div>
-        <div>
-            <input type="text" (cut)="fireEvent($event)" placeholder="cut">
-        </div>
-        <div>
-            <input type="text" (copy)="fireEvent($event)" placeholder="copy">
-        </div>
-        <div>
-            <input type="text" (paste)="fireEvent($event)" placeholder="paste">
-        </div>
-        <hr>
-        <div>
-            <input type="text" (keyup)="changeText($event)" placeholder="change text" value={{ text }}>
-        </div>
-        <h2>{{ text }}</h2>
     `
 })
 
 export class SandboxComponent{
-    text:string = 'Hello';
-    fireEvent(e){
-        console.log(e.type);
-    }
-
-    changeText(e){
-        this.text = e.target.value;
-    }
+    name:string = '';
+    age:number = 0;
 }
