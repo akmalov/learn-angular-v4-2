e781f3d6f0480a83e69cc9d45a361e2eba94c07e
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-12 16:52:06 +0300

    learn observables

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -6,18 +6,18 @@ import { DataService } from '../../services/data.service';
     template:`
         <h1>Hello World</h1>
         <ul class="list-group">
-            <li class="list-group-item" *ngFor="let user of users">
-                {{ user }}
-            </li>
+            <li class="list-group-item" *ngFor="let d of data">{{d}}</li>
         </ul>
     `
 })
 
 export class SandboxComponent{
-    users:string[];
+    data:any[] = [];
 
     constructor(public dataService:DataService)
     {
-        this.users = this.dataService.getUsers();
+        this.dataService.getData().subscribe(data => {
+            this.data.push(data);
+        });
     }
 }
